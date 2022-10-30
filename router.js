5d9d4b1e7b347848ecc6a4cc7ad7b10b1c025a6f
Manel <quemonmanel@gmail.com>
2022-10-30 20:07:41 +0100

    Project04-working with postman

--- router.js
+++ router.js
@@ -1,13 +1,10 @@
-
 const router = require('express').Router();
-
 
 const UsersRouter = require('./views/UsersRouter');
 // const FilmsRouter = require('./views/FilmsRouter');
 // const SeriesRouter = require('./views/SeriesRouter');
 // const OrdersFilmsRouter = require('./views/OrdersFilmsRouter');
 // const OrdersSeriesRouter = require('./views/OrdersSeriesRouter');
-
 
 router.use('/users', UsersRouter);
 // router.use('/films', FilmsRouter);
