458710786afc28893ef7810c81a9fb2a0389553f
Manel <quemonmanel@gmail.com>
2022-11-01 00:05:07 +0100

    Project04-FilmsRouters-CRUD

--- views/FilmsRouters.js
+++ views/FilmsRouters.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
 
-const { Film } = require('../models/index');
+const {Film} = require('../models/index');
+
+
+router.get('/', async (req, res) => {
+
+
+    try {
+
+        let resultado = await Film.findAll();
+        
+        //Si resultado no esta vacia.....
+        if(resultado[0].id !== undefined){
+            res.send(resultado)
+        };
+
+
+    } catch (error) {
+        console.log(error);
+    };
+})
+
 
 
 module.exports = router;
