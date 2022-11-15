0079906ea0c12fe85f08fbb477440fcc74e3cf00
Manel <quemonmanel@gmail.com>
2022-11-15 21:26:42 +0100

    Project04-before railway server

--- seeders/20221106104951-create-users.js
+++ seeders/20221106104951-create-users.js
@@ -59,7 +59,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     
 
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
      
   }
 };
